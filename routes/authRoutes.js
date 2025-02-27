ccecca4a8332def1ac8e84541991d15747618729
sunghyun Kim <constate93@gmail.com>
2025-02-27 16:56:12 +0900

    reset project

--- routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { register, login } = require('../controllers/authController');
-
-router.post('/signup', register);
-router.post('/login', login);
-
-module.exports = router;
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const authRoutes = require('./authRoutes');
-const userRoutes = require('./userRoutes');
-
-router.use('/auth', authRoutes);
-router.use('/users', userRoutes);
-
-module.exports = router;
