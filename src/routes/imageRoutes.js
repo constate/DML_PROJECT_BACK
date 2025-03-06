b3d93352a1952dba6d0cc3ac95a515330f7e6045
sunghyun Kim <constate93@gmail.com>
2025-03-06 13:39:06 +0900

    uploadProductImage -> uploadImage 함수명 변경

--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
-const { uploadProductImage } = require('../controllers/imageController');
+const { uploadImage } = require('../controllers/imageController');
 
 const router = express.Router();
 
-router.post('/image/upload', upload.single('image'), uploadProductImage);
+router.post('/image/upload', upload.single('image'), uploadImage);
 
 module.exports = router;
