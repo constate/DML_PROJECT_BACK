cb73569d9e9cb97b0e7f31b1b5d0937601c61223
sunghyun Kim <constate93@gmail.com>
2025-03-14 19:29:09 +0900

    constants/firebase.js 내 export 문법 수정

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -1,7 +1,9 @@
-export const COLLECTION = {
-    USERS: 'DML_USERS',
-    GROUPS: 'DML_GROUPS',
-    PRODUCTS: 'DML_PRODUCTS',
-    ORDERS: 'DML_ORDERS',
-    IMAGES: 'images',
+module.exports = {
+    COLLECTION: {
+        USERS: 'DML_USERS',
+        GROUPS: 'DML_GROUPS',
+        PRODUCTS: 'DML_PRODUCTS',
+        ORDERS: 'DML_ORDERS',
+        IMAGES: 'images',
+    },
 };
