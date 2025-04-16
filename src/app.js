a826355bbc474a1f4e083814c9d9f90633c8caa6
sunghyun Kim <constate93@gmail.com>
2025-04-16 20:07:14 +0900

    불필요하게 views/ejs 파일을 참조하고 있던 부분 수정

--- src/app.js
+++ src/app.js
@@ -15,24 +15,9 @@ app.use(express.json());
 // 정적 파일 제공을 위한 미들웨어 설정
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 뷰 엔진 설정 (EJS 사용 예시)
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.get('/', (req, res) => {
-    res.render('home'); // views/home.ejs 파일 렌더링
-});
-
-app.get('/signup', (req, res) => {
-    res.render('signup'); // views/signup.ejs 파일 렌더링
-});
-
-app.get('/login', (req, res) => {
-    res.render('login'); // views/login.ejs 파일 렌더링
-});
-
-app.get('/addproduct', (req, res) => {
-    res.render('addproduct'); // views/addproduct.ejs 파일 렌더링
+// 테스트용 엔드포인트
+app.get('/api/test', (req, res) => {
+    res.json({ data: 'test' });
 });
 
 // API 라우트 설정
@@ -40,10 +25,5 @@ app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', productRoutes);
 app.use('/api', imageRoutes);
-
-// 테스트용 엔드포인트
-app.get('/api/test', (req, res) => {
-    res.json({ data: 'test' });
-});
 
 module.exports = app;
