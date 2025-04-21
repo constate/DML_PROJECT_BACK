04d39cbbc5dc5cf36e3470e14a12e3b774166464
sunghyun Kim <constate93@gmail.com>
2025-04-21 13:38:43 +0900

    groupRoutes 적용

--- src/app.js
+++ src/app.js
@@ -4,12 +4,21 @@ const path = require('path');
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
+const groupRoutes = require('./routes/groupRoutes');
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const imageRoutes = require('./routes/imageRoutes');
 
 const app = express();
-app.use(cors());
+// const allowedOrigin = 'http://192.168.0.12:1234';
+const allowedOrigin = 'http://192.168.55.104:1234';
+
+app.use(
+    cors({
+        origin: allowedOrigin,
+        credentials: true, // 👉 쿠키/헤더를 포함한 요청 허용
+    }),
+);
 app.use(express.json());
 
 // 정적 파일 제공을 위한 미들웨어 설정
@@ -22,6 +31,7 @@ app.get('/api/test', (req, res) => {
 
 // API 라우트 설정
 app.use('/api', authRoutes);
+app.use('/api', groupRoutes);
 app.use('/api', userRoutes);
 app.use('/api', productRoutes);
 app.use('/api', imageRoutes);
